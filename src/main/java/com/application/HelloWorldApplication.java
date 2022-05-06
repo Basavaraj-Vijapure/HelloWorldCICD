377b158014c96034c0b845829330dda86f5cce28
Basavaraj Vijapure <basavavijapure0206@gmail.com>
2022-05-06 13:33:36 +0530

    My First Commit

---
+++ src/main/java/com/application/HelloWorldApplication.java
@@ -0,0 +1,16 @@
+package com.application;
+
+public class HelloWorldApplication {
+	
+	
+	public void m1() {
+		
+		System.out.println("Hello World Application Is Running............");
+	}
+
+	public static void main(String[] args) {
+		
+		HelloWorldApplication helloWorldApplication = new HelloWorldApplication();
+		
+	}
+}
